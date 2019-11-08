990b63b7a27b87f761e389e5e8f175e9df730a99
Yuseunggeun <yuu3730@naver.com>
2019-11-09 00:30:40 +0900

    plus alpha(3)

--- alpha.py
+++ alpha.py
@@ -22,11 +22,14 @@ class Player():
         self.intelligence = 1
         self.charm = 1
         self.money = 10
+        self.maxmoney = 200000
         self.happiness = 0
+        self.stamina = 100
+
 player = Player()
 
 
-currentscene = 0 #현재 secne 번호
+currentscene = 10 #현재 secne 번호
 class Image(pg.sprite.Sprite): #rect는 sprite객체만 가능해서 만듦
     def __init__(self,fileroute,size,rect): #size랑 rect는 튜플
         pg.sprite.Sprite.__init__(self)
@@ -35,13 +38,15 @@ class Image(pg.sprite.Sprite): #rect는 sprite객체만 가능해서 만듦
             self.image = (pg.transform.scale(self.image,size)).convert_alpha()
         self.Rect = self.image.get_rect(topleft=rect)
 
+intro_background = Image('./Image/Intro_background.png',(850,650),(-30,-15))
 #---0---
 main_background = Image('./Image/main_background.png',-1,(0,0))
 go_alba = Image('./Image/go_alba.png',-1,(18,68))
 go_stat = Image('./Image/go_stat.png',-1,(18,180))
-
 #---1---
-ui = Image('./Image/a_ui.png',-1,(0,444)) 
+ui = Image('./Image/a_ui2.png',-1,(0,444)) 
+stamina_gauge = Image('./Image/stamina_gauge.png',-1,(625,541)) #width=158 height=25
+
 alba1_1 = Image('./Image/alba1_1.png',-1,(95,50)) 
 alba1_2 = Image('./Image/alba1_2.png',-1,(95,175)) 
 alba1_3 = Image('./Image/alba1_3.png',-1,(95,300))
@@ -60,27 +65,72 @@ heal1 = Image('./Image/heal1.png',-1,(95,50))
 heal2 = Image('./Image/heal2.png',-1,(95,175)) 
 heal3 = Image('./Image/heal3.png',-1,(95,300))
 
-scenelist = [[],[],[],[],[],[],[],[],[]] 
-scenelist[0].extend([main_background,ui,go_alba,go_stat])
-scenelist[1].extend([ui,alba1_1,alba1_2,alba1_3,backmove,frontmove])
-scenelist[2].extend([ui,alba2_1,alba2_2,alba2_3,backmove,frontmove])
-scenelist[4].extend([ui,int1,int2,int3,backmove,frontmove])
-scenelist[5].extend([ui,heal1,heal2,heal3,backmove,frontmove])
+scenelist = [[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]] 
+scenelist[0].extend([main_background,go_alba,go_stat])
+scenelist[1].extend([alba1_1,alba1_2,alba1_3,backmove,frontmove])
+scenelist[2].extend([alba2_1,alba2_2,alba2_3,backmove,frontmove])
+scenelist[4].extend([int1,int2,int3,backmove,frontmove])
+scenelist[5].extend([heal1,heal2,heal3,backmove,frontmove])
+scenelist[10].extend([intro_background]) #인트로
 
 def scene_change_blit(currentscene): 
     for sprite in scenelist[currentscene]:
         screen.blit(sprite.image,sprite.Rect)
 
+def ui_blit(currentscene): #ui출력용,currentscene받아서 특정씬에는 안돌리게 하면됨
+    screen.blit(ui.image,ui.Rect)
+    screen.blit(stamina_gauge.image,stamina_gauge.Rect)
+
+    realtime_Font = pg.font.Font(None, 25)
+    text = realtime_Font.render(str(player.money),True,(0,0,0))
+    textrect = text.get_rect()
+    textrect.topleft =(343,506)
+    screen.blit(text,textrect)
+
+    text = realtime_Font.render(str(player.health),True,(0,0,0))
+    textrect = text.get_rect()
+    textrect.topleft =(127,470)
+    screen.blit(text,textrect)
+
+    text = realtime_Font.render(str(player.intelligence),True,(0,0,0))
+    textrect = text.get_rect()
+    textrect.topleft =(127,518)
+    screen.blit(text,textrect)
+
+    text = realtime_Font.render(str(player.maxmoney),True,(0,0,0))
+    textrect = text.get_rect()
+    textrect.topleft =(626,479)
+    screen.blit(text,textrect)
+
+
+def stamina_update(player,stamina_gauge): #스태미나 게이지 조절용
+    if player.stamina <= 0:
+        player.stamina = 0
+    stamina_gauge.image = pg.transform.scale(stamina_gauge.image,(int(158*player.stamina/100),25))
+
+def money_decline(player,decline): #돈없으면 업글못해
+    if player.money < decline:
+        return False
+    else:
+        player.money -= decline
+        return True
+
 while True:
     screen.fill(white)
 
-    realtime_Font = pg.font.Font(None, 25)
+    stamina_update(player,stamina_gauge)
+
     scene_change_blit(currentscene)
+
+    ui_blit(currentscene)
 
     for event in pg.event.get():
         if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
             pos = pg.mouse.get_pos()
-            if currentscene == 0:
+            if currentscene == 10:
+                if intro_background.Rect.collidepoint(pos):
+                    currentscene = 0
+            elif currentscene == 0:
                 if go_alba.Rect.collidepoint(pos):
                     currentscene = 1
                 elif go_stat.Rect.collidepoint(pos):
@@ -107,34 +157,30 @@ while True:
                     currentscene = 1
             elif currentscene == 4:
                 if int1.Rect.collidepoint(pos):
-                    player.money -= 30000
-                    player.intelligence += 1
+                    player.intelligence += 1 * money_decline(player,30000)
                 elif int2.Rect.collidepoint(pos):
-                    player.money -= 1000000
-                    player.intelligence += 5
+                    player.intelligence += 5 * money_decline(player,1000000)
                 elif int3.Rect.collidepoint(pos):
-                    player.money -= 10000000
-                    player.intelligence += 10
+                    player.intelligence += 50 * money_decline(player,1000000)
                 elif backmove.Rect.collidepoint(pos):
                     currentscene = 0   
                 elif frontmove.Rect.collidepoint(pos):
                     currentscene = 5 
             elif currentscene == 5:
-                if int1.Rect.collidepoint(pos):
-                    player.money -= 50000
-                    player.health += 1
-                elif int2.Rect.collidepoint(pos):
-                    player.money -= 2500000
-                    player.health += 5
-                elif int3.Rect.collidepoint(pos):
-                    player.money -= 50000000
-                    player.health += 10
+                if heal1.Rect.collidepoint(pos):
+                    player.health += 1 * money_decline(player,50000)
+                elif heal2.Rect.collidepoint(pos):
+                    player.health += 5 * money_decline(player,2500000)
+                elif heal3.Rect.collidepoint(pos):
+                    player.health += 10 * money_decline(player,50000000)
                 elif backmove.Rect.collidepoint(pos):
                     currentscene = 4    
         if event.type == pg.QUIT:
             pg.quit()
             quit()
  
+    #test용
+    realtime_Font = pg.font.Font(None, 25)
     text = realtime_Font.render('cursor = '+str(pg.mouse.get_pos()),True,(0,0,0))
     textrect = text.get_rect()
     textrect.topleft =(0,0)
@@ -145,20 +191,6 @@ while True:
     textrect.topleft =(700,0)
     screen.blit(text,textrect)
 
-    text = realtime_Font.render(str(player.money),True,(0,0,0))
-    textrect = text.get_rect()
-    textrect.topleft =(343,506)
-    screen.blit(text,textrect)
-
-    text = realtime_Font.render(str(player.health),True,(0,0,0))
-    textrect = text.get_rect()
-    textrect.topleft =(127,470)
-    screen.blit(text,textrect)
-
-    text = realtime_Font.render(str(player.intelligence),True,(0,0,0))
-    textrect = text.get_rect()
-    textrect.topleft =(127,518)
-    screen.blit(text,textrect)
 
     pg.display.update()
     clock.tick(FPS)
